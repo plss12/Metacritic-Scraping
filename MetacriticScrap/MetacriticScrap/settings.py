5cf3730d50c6c353a1d1b2def052a1c87f10e1d0
Pedro Luis Soto Santos <pepoluis712@gmail.com>
2023-04-23 01:16:37 +0200

    Update settings.py

--- MetacriticScrap/MetacriticScrap/settings.py
+++ MetacriticScrap/MetacriticScrap/settings.py
@@ -112,6 +112,6 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
-if not DEBUG:
-    STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-    STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+#if not DEBUG:
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
